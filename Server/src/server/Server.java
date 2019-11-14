a7cbff8afbc5ce9234897cf31f3a724a188d1f78
manassa97@hotmail.com <david@Acer-David>
2019-11-14 02:20:15 +0000

    Server:
    Já é possivel testar o envio de mensagens entre clientes. Usem o comando:
    tosend%<id_para_o_qual_querem_enviar>%<mensagem>
    ex: tosend%1%Olá

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -8,14 +8,14 @@ public class Server {
     
     public static Server ms;
     
-    public static HashMap<Integer, Socket> connect_list = new HashMap<>();
+    public static HashMap<Integer, ClientHandler> connect_list = new HashMap<>();
 	
     public static void main(String[] args) throws IOException {
             
         int id_client = 0;
         
-        // server is listening on port 7000
-        ServerSocket ss = new ServerSocket(7000); 
+        // server is listening on port 5067
+        ServerSocket ss = new ServerSocket(5067); 
 
         // running infinite loop for getting 
         // client request 
@@ -29,21 +29,23 @@ public class Server {
                 s = ss.accept(); 
                 
                 ++id_client;
-                connect_list.put(id_client, s); // Put client in the list
+                 // Put client in the list
                 
-                System.out.println("A new client (id = " + String.valueOf(id_client) + ") is connected : " + s);
+                System.out.println("A new client (id = " + String.valueOf(id_client) + ") is connected: " + s + ".");
                 
                 // obtaining input and out streams 
                 DataInputStream dis = new DataInputStream(s.getInputStream()); 
                 DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
 
-                System.out.println("Assigning new thread for this client"); 
+                System.out.println("Assigning new thread for this client."); 
 
-                // create a new thread object
-                Thread t = new ClientHandler(s, dis, dos,id_client); //, Server
-
-                // Invoking the start() method 
-                t.start(); 
+                
+                ClientHandler ch = new ClientHandler(s, dis, dos, id_client);
+                Thread t = ch;
+                t.start();
+                
+                connect_list.put(id_client, ch);
+                
 
             } catch (Exception e) {
                 s.close(); 
@@ -83,9 +85,9 @@ class ClientHandler extends Thread {
         
         while (true) {
             
-            try { 
+            try {
 
-                // Ask user what he wants
+                /// Just a welcome message
                 if (first) { first = false;
                     dos.writeUTF("Your ID = "+ String.valueOf(id_client) +"\n Type 'exit' to terminate connection."); 
                 }
@@ -96,36 +98,47 @@ class ClientHandler extends Thread {
                 if(received.equalsIgnoreCase("exit")) {
                     System.out.println("Client " + this.s + " sends exit..."); 
                     System.out.println("Closing this connection."); 
-                    this.s.close(); 
-                    System.out.println("Connection closed"); 
-                    break; 
-                } 
+                    this.s.close();
+                    System.out.println("Connection closed."); 
+                    break;
+                }
 
-                // creating Date object 
-                Date date = new Date(); 
+                // write on output stream based on the
+                // answer from the client
+                if (received.startsWith("tosend%")) {
+                    
+                    int toID = Integer.parseInt(received.split("%")[1]);
+                    String message = received.split("%")[2];
+                    
+                    Server.connect_list.get(toID).dos.writeUTF("Message recived from " + id_client + ": " + message);
+                    dos.writeUTF("Message send to " + toID + ": " + message);
+                    
+                } else {
+                    switch (received) {
 
-                // write on output stream based on the 
-                // answer from the client 
-                switch (received) {
+                        case "list": // Get a list of connected users
+                            dos.writeUTF(Server.connect_list.keySet().toString());
+                            break; 
 
-                    case "list": // Get a list of connected users
-                        dos.writeUTF(Server.connect_list.keySet().toString());
-                        break; 
+                        case "id": // Get client ID
+                            dos.writeUTF("Your ID = " + id_client + ".");
+                            break;
 
-                    case "id": // Get client ID
-                        dos.writeUTF("Your ID = " + id_client + "."); 
-                        break; 
+                        case "sendmessage":
+                            dos.writeUTF("Select ID: ");
+                            break;
 
-                    default: 
-                        dos.writeUTF("Invalid input"); 
-                        break;
-                            
+                        default:
+                            dos.writeUTF("Invalid input.");
+                            break;
+
+                    }
                 }
                 
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
-        } 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         try {
             // closing resources 
